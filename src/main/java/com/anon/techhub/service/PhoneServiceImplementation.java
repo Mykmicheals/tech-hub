45cbae094f12378e55ceaeb666eb0bce3e06a3b2
amMicheals <mandrew@insight7.io>
2019-06-03 08:41:05 +0100

    code refactoring

--- src/main/java/com/anon/techhub/service/PhoneServiceImplementation.java
+++ src/main/java/com/anon/techhub/service/PhoneServiceImplementation.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+
 public class PhoneServiceImplementation implements PhoneService{
     PhoneDao phoneService;
     @Autowired
